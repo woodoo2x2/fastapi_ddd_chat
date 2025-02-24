7dd031329343f9e884d649445d30dd3c81fc80ca
woodoo2x2 <woodoo2x2@gmail.com>
2025-02-24 22:55:25 +0300

    fix producer name event

--- app/infrastructure/message_brokers/kafka.py
+++ app/infrastructure/message_brokers/kafka.py
@@ -11,6 +11,7 @@ class KafkaMessageBroker(BaseMessageBroker):
     # consumer: AIOKafkaConsumer
 
     async def send_message(self, topic: str, value: bytes) -> None:
+
         await self.producer.start()
         await self.producer.send_and_wait(topic=topic, value=value)
 
